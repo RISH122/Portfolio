79db0eb3d9086fdaccfb7afefb50d62571aa60c0
Rishi Dubey <rishidubey652@gmail.com>
2024-08-08 16:07:46 +0530

    Second Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
            Web Developer&nbsp;
-            <span className='sm:block hidden'> | Competative Programing Enthusiast</span>
+            <span className='sm:block hidden'> | Competative Programming Enthusiast</span>
           </p>
         </Link>
 
